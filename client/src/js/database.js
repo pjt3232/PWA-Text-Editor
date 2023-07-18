973dee01e180851e6c16973d1c036ef3b10e38ac
Tranel <tranel@Tranels-MacBook-Air.local>
2023-07-18 18:16:54 -0500

    Added database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,6 @@
 import { openDB } from 'idb';
 
+//intiates the jate database
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -12,10 +13,26 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id:1, value:content });
+  const result = await request;
+  console.log('Data saved to the database', result.value);
+}
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET from the database');
+  const db = await openDB('jate');
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  console.log('Data retrieved from the database', result.value);
+}
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,14 +1,13 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
     deferredPrompt = event;
     butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+// Click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     if (deferredPrompt) {
         deferredPrompt.prompt();
@@ -17,7 +16,7 @@ butInstall.addEventListener('click', async () => {
     }
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// Handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     window.deferredPrompt = null;
     console.log('App installed!');
